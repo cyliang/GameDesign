f8ebdc0c1309439d52bc1caaa530855bf7405382
Chih-Yung Liang <r05944012@csie.ntu.edu.tw>
2017-10-12 06:11:24 +0800

    Hw1: Rotate camera list.

---
+++ homework1/Assets/Scripts/HighCamBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighCamBehavior : MonoBehaviour {
+
+	public GameObject player;
+	public float height;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = Vector3.Scale (
+			player.transform.position,
+			new Vector3 (1f, 0, 1f)
+		) + Vector3.up * height;
+	}
+}
