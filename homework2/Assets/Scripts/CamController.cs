c4a2742bb867a04c02fb996e05c045fa43b09da2
Chih-Yung Liang <r05944012@csie.ntu.edu.tw>
2017-11-01 20:45:32 +0800

    hw2: copied from quiz3

---
+++ homework2/Assets/Scripts/CamController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour {
+
+	public Camera[] cameraList;
+	private int lastIndex;
+
+	void Start() {
+		lastIndex = 0;
+
+		for (int i = lastIndex + 1; i < cameraList.Length; i++) {
+			cameraList [i].enabled = false;
+		}
+	}
+
+	public void rotateCamera() {
+		cameraList [lastIndex].enabled = false;
+
+		++lastIndex;
+		lastIndex %= cameraList.Length;
+
+		cameraList [lastIndex].enabled = true;
+	}
+}
