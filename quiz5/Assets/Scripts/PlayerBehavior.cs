134cfb01f830b716f041b23c8c7cbb9e4d4e6264
Chih-Yung Liang <r05944012@csie.ntu.edu.tw>
2017-11-09 20:03:50 +0800

    quiz5: rotate player and hand

--- quiz5/Assets/Scripts/PlayerBehavior.cs
+++ quiz5/Assets/Scripts/PlayerBehavior.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerBehavior : MonoBehaviour {
 
 	public float moveSpeed, jumpSpeed;
+	public GameObject hand;
 
 	private Rigidbody2D rb;
 
@@ -20,5 +21,16 @@ public class PlayerBehavior : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.Space) && rb.velocity.y == 0f) {
 			rb.velocity += Vector2.up * jumpSpeed;
 		}
+
+		Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mouse.z = 0;
+		transform.rotation = Quaternion.Euler(
+			mouse.x >= transform.position.x ?
+				Vector3.zero :
+				Vector3.up * 180
+		);
+
+		Vector3 handPivot = hand.transform.position;
+		hand.transform.rotation = Quaternion.FromToRotation (Vector3.right, mouse - handPivot);
 	}
 }
