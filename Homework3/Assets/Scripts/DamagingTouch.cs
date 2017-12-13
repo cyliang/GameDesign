3fa124542c84bb556acbc9f55fe16b063edfc373
Chih-Yung Liang <r05944012@csie.ntu.edu.tw>
2017-12-14 03:56:38 +0800

    hw3: UI & HP damaging

---
+++ Homework3/Assets/Scripts/DamagingTouch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagingTouch : MonoBehaviour {
+
+	public float hurt;
+	public GameController gameController;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		gameController.MakeDamage (hurt);
+	}
+}
--- Homework3/Assets/Scripts/GameController.cs
+++ Homework3/Assets/Scripts/GameController.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
 	public float floatingSpeed;
 	public int stairPeriod;
 
+	public float maxHP;
+
+	public Slider HPSlider;
+	public Text stageText, recordText;
+
 	public StairGenerator stairGenerator;
 
+	float HP;
+	int stage, record;
 	int count = 0;
+
+	void Start () {
+		replay ();
+	}
 		
 	void Update () {
 		FloatingObject.Float (floatingSpeed * Time.deltaTime);
@@ -18,8 +30,38 @@ public class GameController : MonoBehaviour {
 	void FixedUpdate() {
 		if (count % stairPeriod == 0) {
 			stairGenerator.genStairAtRandom ();
+			stage++;
+			updateInfo ();
 		}
 
 		count++;
+	}
+
+	public void MakeDamage(float damage) {
+		HP -= damage;
+		updateInfo ();
+
+		if (HP <= 0f) {
+			dead ();
+		}
+	}
+
+	void dead() {
+		print ("DEAD");
+	}
+
+	void replay() {
+		HP = maxHP;
+		HPSlider.minValue = 0f;
+		HPSlider.maxValue = maxHP;
+
+		stage = 0;
+		updateInfo ();
+	}
+
+	void updateInfo() {
+		HPSlider.value = HP;
+		stageText.text = stage.ToString();
+		recordText.text = record.ToString();
 	}
 }
