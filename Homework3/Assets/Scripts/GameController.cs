cd9cc9e3ed7ae14c97c95fd342a9edf5ce611f46
Chih-Yung Liang <r05944012@csie.ntu.edu.tw>
2017-12-14 04:22:23 +0800

    hw3: UI, basic gaming

--- Homework3/Assets/Scripts/GameController.cs
+++ Homework3/Assets/Scripts/GameController.cs
@@ -11,23 +11,33 @@ public class GameController : MonoBehaviour {
 	public float maxHP;
 
 	public Slider HPSlider;
-	public Text stageText, recordText;
+	public Text stageText, recordText, recordText2;
 
+	public GameObject player;
+	public GameObject gameStopPanel;
 	public StairGenerator stairGenerator;
 
+	bool isPlaying = false;
 	float HP;
 	int stage, record;
 	int count = 0;
+	Vector3 playerStartPos;
 
 	void Start () {
-		replay ();
+		playerStartPos = player.transform.position;
 	}
 		
 	void Update () {
+		if (!isPlaying)
+			return;
+		
 		FloatingObject.Float (floatingSpeed * Time.deltaTime);
 	}
 
 	void FixedUpdate() {
+		if (!isPlaying)
+			return;
+		
 		if (count % stairPeriod == 0) {
 			stairGenerator.genStairAtRandom ();
 			stage++;
@@ -46,17 +56,40 @@ public class GameController : MonoBehaviour {
 		}
 	}
 
+	public void startPlay() {
+		isPlaying = true;
+		gameStopPanel.SetActive (false);
+		replay ();
+	}
+
 	void dead() {
-		print ("DEAD");
+		isPlaying = false;
+		gameStopPanel.SetActive (true);
+		player.SetActive (false);
+		record = Mathf.Max (record, stage);
+		recordText2.text = record.ToString ();
 	}
 
 	void replay() {
+		// Data reset
 		HP = maxHP;
 		HPSlider.minValue = 0f;
 		HPSlider.maxValue = maxHP;
-
 		stage = 0;
 		updateInfo ();
+
+		// Scene reset
+		foreach (Transform stair in stairGenerator.transform) {
+			Destroy (stair.gameObject);
+		}
+		Instantiate (
+			stairGenerator.stairPrefab,
+			new Vector2 (0f, -2.4f),
+			Quaternion.identity,
+			stairGenerator.transform
+		);
+		player.SetActive (true);
+		player.transform.position = playerStartPos;
 	}
 
 	void updateInfo() {
