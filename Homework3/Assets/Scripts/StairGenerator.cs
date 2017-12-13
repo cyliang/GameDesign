e6247db286d08581dc62420d69291aa68501b2e5
Chih-Yung Liang <r05944012@csie.ntu.edu.tw>
2017-12-13 23:48:42 +0800

    Hw3: Scene & Stair generator

---
+++ Homework3/Assets/Scripts/StairGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairGenerator : MonoBehaviour {
+
+	public Vector2 genPosBegin, genPosEnd;
+	public GameObject stairPrefab;
+
+	public GameObject genStairAtRandom() {
+		return Instantiate (
+		    stairPrefab,
+		    Vector2.Lerp (genPosBegin, genPosEnd, Random.value),
+		    Quaternion.identity,
+		    transform
+		);
+	}
+}
